80188d230b3a813decb265bd41b8d53360b36ba9
Andrew <manloengchung@yahoo.co.uk>
2019-05-15 23:07:56 +0100

    Added Some JS code, but commented out for later

--- test-site/scripts/script.js
+++ test-site/scripts/script.js
@@ -1,18 +1,14 @@
-/* 
+/*
+// Make Changes to Paragraphs
 
-Stuff i could implement:
+document.getElementById("homePara").innerHTML = "Hi There";
+document.getElementById("portfolioPara").innerHTML = "Hi There 2";
+document.getElementById("aboutPara1").innerHTML = "Hi There 3";
+document.getElementById("aboutPara2").innerHTML = "Hi There 4";
+document.getElementById("aboutPara3").innerHTML = "Hi There 5";
 
--night mode
--e-mail validator
--vistor counter?
--spotify music
--site search
-
--what i'm up to
-
-
+// Make changes to Images on about me
+document.getElementById("aboutImg1").src = "#";
+document.getElementById("aboutImg2").src = "#";
+document.getElementById("aboutImg3").src = "#";
 */
-
-
-
-
