a32a6ffc5cc931f06966f4708adeacf6dfc736c3
Andrew <manloengchung@yahoo.co.uk>
2019-05-17 14:45:15 +0100

    Started applying js into web page

--- test-site/scripts/script.js
+++ test-site/scripts/script.js
@@ -1,14 +1,18 @@
 /*
-// Make Changes to Paragraphs
-
-document.getElementById("homePara").innerHTML = "Hi There";
-document.getElementById("portfolioPara").innerHTML = "Hi There 2";
-document.getElementById("aboutPara1").innerHTML = "Hi There 3";
-document.getElementById("aboutPara2").innerHTML = "Hi There 4";
-document.getElementById("aboutPara3").innerHTML = "Hi There 5";
 
 // Make changes to Images on about me
 document.getElementById("aboutImg1").src = "#";
 document.getElementById("aboutImg2").src = "#";
 document.getElementById("aboutImg3").src = "#";
 */
+
+// Make Changes to Paragraphs
+let para = document.getElementsByTagName("p");
+
+for (let i = 0; i < para.length; i++) {
+  if (para[i].innerHTML.length > 165) {
+    para[i].innerHTML = para[i].innerHTML.slice(0, 165) + "...";
+  }
+}
+
+console.log($(window).width());
