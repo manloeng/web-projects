08eb3a101f81922fdf8853d1249ccfda06c102c2
Andrew <manloengchung@yahoo.co.uk>
2019-05-18 10:47:42 +0100

    made small changes

--- test-site/scripts/script.js
+++ test-site/scripts/script.js
@@ -9,10 +9,9 @@ document.getElementById("aboutImg3").src = "#";
 // Make Changes to Paragraphs
 let para = document.getElementsByTagName("p");
 
+//if ($(window).width() < 900) - require to automatically adjust length
 for (let i = 0; i < para.length; i++) {
   if (para[i].innerHTML.length > 165) {
     para[i].innerHTML = para[i].innerHTML.slice(0, 165) + "...";
   }
 }
-
-console.log($(window).width());
