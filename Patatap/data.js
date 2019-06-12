58bc74b6a784adf3758627efbc9c0833101991dc
Andrew <manloengchung@yahoo.co.uk>
2019-06-12 13:59:20 +0100

    Added sound to all keys

---
+++ Patatap/data.js
@@ -0,0 +1,166 @@
+var keyData = {
+	a: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/bubbles.mp3' ]
+		})
+	},
+	b: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/clay.mp3' ]
+		})
+	},
+	c: {
+		color: 'green',
+		sound: new Howl({
+			src: [ 'sounds/confetti.mp3' ]
+		})
+	},
+	d: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/corona.mp3' ]
+		})
+	},
+	e: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/dotted-spiral.mp3' ]
+		})
+	},
+	f: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/flash-1.mp3' ]
+		})
+	},
+	g: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/flash-2.mp3' ]
+		})
+	},
+	h: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/flash-3.mp3' ]
+		})
+	},
+	i: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/glimmer.mp3' ]
+		})
+	},
+	j: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/moon.mp3' ]
+		})
+	},
+	k: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/pinwheel.mp3' ]
+		})
+	},
+	l: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/piston-1.mp3' ]
+		})
+	},
+	m: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/piston-2.mp3' ]
+		})
+	},
+	n: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/piston-3.mp3' ]
+		})
+	},
+	o: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/prism-1.mp3' ]
+		})
+	},
+	p: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/prism-2.mp3' ]
+		})
+	},
+	q: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/prism-3.mp3' ]
+		})
+	},
+	r: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/splits.mp3' ]
+		})
+	},
+	s: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/squiggle.mp3' ]
+		})
+	},
+	t: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/bubbles.mp3' ]
+		})
+	},
+	u: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/strike.mp3' ]
+		})
+	},
+	v: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/suspension.mp3' ]
+		})
+	},
+	w: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/timer.mp3' ]
+		})
+	},
+	d: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/ufo.mp3' ]
+		})
+	},
+	x: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/viel.mp3' ]
+		})
+	},
+	y: {
+		color: 'purple',
+		sound: new Howl({
+			src: [ 'sounds/wipe.mp3' ]
+		})
+	},
+	z: {
+		color: 'yellow',
+		sound: new Howl({
+			src: [ 'sounds/zig-zag.mp3' ]
+		})
+	}
+};
+
+module.exports = keyData;
