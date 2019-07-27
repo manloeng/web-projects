f51c4d7c431900fe7fe60656180cd367f6e8abed
Andrew <manloengchung@yahoo.co.uk>
2019-07-27 18:35:25 +0100

    fixing small bugs

--- preactDemo/my-demo-project/src/routes/home/index.js
+++ preactDemo/my-demo-project/src/routes/home/index.js
@@ -5,17 +5,18 @@ const Home = ({ data }) => (
 	<div class={style.home}>
 		<h1>Welcome To NC News</h1>
 		<p>This is the Home page.</p>
-		{Object.keys(data.routes).map((key) => (
-			<div>
-				<p>{key}</p>
-				<p>{data.routes[key].description}</p>
-				<p>{JSON.stringify(data.routes[key]).exampleResponse}</p>
-				{/* feels very nested */}
-				{/* {console.log(data.routes[key].exampleResponse)} */}
-				{/* {data.routes[key].exampleResponse && <p>{JSON.stringify(data.routes[key].exampleResponse.topic)}</p>} */}
-				}
-			</div>
-		))}
+		{data &&
+			Object.keys(data.routes).map((key) => (
+				<div>
+					<p>{key}</p>
+					<p>{data.routes[key].description}</p>
+					<p>{JSON.stringify(data.routes[key]).exampleResponse}</p>
+					{/* feels very nested */}
+					{/* {console.log(data.routes[key].exampleResponse)} */}
+					{/* {data.routes[key].exampleResponse && <p>{JSON.stringify(data.routes[key].exampleResponse.topic)}</p>} */}
+					}
+				</div>
+			))}
 	</div>
 );
 
--- preactDemo/my-demo-project/src/routes/users/index.js
+++ preactDemo/my-demo-project/src/routes/users/index.js
@@ -3,14 +3,15 @@ const Users = ({ data }) => (
 		<h1>Welcome To our selection of Users</h1>
 		<p>This is the Users page.</p>
 
-		{data.users.map((user) => (
-			<div>
-				<h2>{user.username}</h2>
-				<p>{user.name}</p>
-				{/* bad avatar url */}
-				<img src={`${user.avatar_url}`} />
-			</div>
-		))}
+		{data &&
+			data.users.map((user) => (
+				<div>
+					<h2>{user.username}</h2>
+					<p>{user.name}</p>
+					{/* bad avatar url */}
+					<img src={`${user.avatar_url}`} />
+				</div>
+			))}
 	</div>
 );
 
