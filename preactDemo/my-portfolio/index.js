7198cbb440f362f5108a191b28de9306b468e63d
Andrew <manloengchung@yahoo.co.uk>
2019-07-28 12:53:23 +0100

    added basic components to set up web page

--- preactDemo/my-portfolio/index.js
+++ preactDemo/my-portfolio/index.js
@@ -1,11 +1,22 @@
 import './style';
 import { Component } from 'preact';
+import Header from './components/Header';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import data from './src/myInfo.js';
 
 export default class App extends Component {
+	state = {
+		data
+	};
+
 	render() {
+		const { name, aboutMe, imgs, myWork } = this.state.data.myInfo;
 		return (
 			<div>
-				<h1>Hello, World!</h1>
+				<Header />
+				<Body />
+				<Footer />
 			</div>
 		);
 	}
