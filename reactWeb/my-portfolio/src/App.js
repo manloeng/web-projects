644dcce1b6226a614724a57209c43fbedbf7795e
Andrew <manloengchung@yahoo.co.uk>
2019-08-01 20:18:12 +0100

    restarting  portfolio design

--- reactWeb/my-portfolio/src/App.js
+++ reactWeb/my-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header';
+import { Router } from '@reach/router';
+import Header from './components/Header';
 
 function App() {
 	return (
