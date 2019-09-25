20e8f8f2fbf3f28773844b5d0872d28e25942983
Andrew <manloengchung@yahoo.co.uk>
2019-09-25 22:16:21 +0100

    remove comments

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app">
-    <p>Hi</p>
     <nav>
-        <p>Hello</p>
         <router-link to='/'>Home</router-link>
-        <router-link to='/register'>Register</router-link>
-        <router-link to='/login'>Login</router-link>
+        <router-link to='/cards'>Cards</router-link>
+        <!-- <router-link to='/register'>Register</router-link> -->
+        <!-- <router-link to='/login'>Login</router-link> -->
     </nav>
     <router-view />
   </div>
@@ -15,3 +14,18 @@
 export default {
 }
 </script>
+
+<style>
+  html, body, #app{
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+  }
+
+  nav {
+    border: 1px solid red;
+    padding: 1rem;
+  }
+</style>
