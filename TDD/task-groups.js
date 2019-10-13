be041d0181426e95478f6a6088b54052775cc400
Andrew <manloengchung@yahoo.co.uk>
2019-10-13 17:13:37 +0100

    Create Hard coded data

---
+++ TDD/task-groups.js
@@ -0,0 +1,27 @@
+module.exports = {
+  Todo: [
+    {
+      id: 1,
+      task: 'Tasks',
+      length: '2 weeks',
+      startDate: '01/03/2019',
+      colour: 'blue',
+    },
+    {
+      id: 2,
+      task: 'Chat',
+      length: '1.5 months',
+      startDate: '16/03/2019',
+      colour: 'red',
+    },
+    {
+      id: 3,
+      task: 'Time Tracking',
+      length: '1 month',
+      startDate: '28/03/2019',
+      colour: 'orange',
+    }
+  ],
+  'In Progress': [],
+  Complete: [],
+};
